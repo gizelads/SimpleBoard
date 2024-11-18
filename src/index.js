7dcaf187c9e0cdd4e2e19f9fc788339040e3da5f
Gizela Delgado Soto <74159414+gizelads@users.noreply.github.com>
2024-11-17 21:20:28 -0400

    use mergeAll to start painting with events of mousedown and mousemove

--- src/index.js
+++ src/index.js
@@ -2,31 +2,37 @@ import {
   fromEvent
 } from 'rxjs'
 import {
-  map
+  map,
+  mergeAll
 } from 'rxjs/operators'
 
 const canvas = document.getElementById('reactive-canvas');
 const canvasContext = canvas.getContext('2d');
+canvasContext.strokeStyle = 'white';
+canvasContext.lineWidth = '5';
 const restartButton = document.getElementById('restart-button');
-
 const cursorPosition = {x: 0, y: 0};
-const onMouseDown$ = fromEvent(canvas, 'mousedown').pipe(map(event => {
-    cursorPosition.x = event.clientX - canvas.offsetLeft;
-    cursorPosition.y = event.clientY - canvas.offsetTop;
-  })
-);
-const onMouseUp$ = fromEvent(canvas, 'mouseup');
 
-onMouseDown$.subscribe();
-
-function drawLine(xi, yi, xf, yf, canvasContext) {
+const updateCursorPosition = (event) => {
+  cursorPosition.x = event.clientX - canvas.offsetLeft;
+  cursorPosition.y = event.clientY - canvas.offsetTop;
+};
+const paintStroke = (event) => {
   canvasContext.beginPath();
-  canvasContext.strokeStyle = 'white';
-  canvasContext.lineWidth = '8';
-  canvasContext.moveTo(xi, yi);
-  canvasContext.lineTo(xf, yf);
+  canvasContext.moveTo(cursorPosition.x, cursorPosition.y);
+  updateCursorPosition(event);
+  canvasContext.lineTo(cursorPosition.x, cursorPosition.y);
   canvasContext.stroke();
   canvasContext.closePath();
-}
+};
 
-drawLine(0, 0, 200, 200, canvasContext);
+const onMouseDown$ = fromEvent(canvas, 'mousedown');
+const onMouseMove$ = fromEvent(canvas, 'mousemove');
+const onMouseUp$ = fromEvent(canvas, 'mouseup');
+const startPaint$ = onMouseDown$.pipe(
+  map(() => onMouseMove$),
+  mergeAll()
+);
+
+onMouseDown$.subscribe(updateCursorPosition);
+startPaint$.subscribe(paintStroke);
